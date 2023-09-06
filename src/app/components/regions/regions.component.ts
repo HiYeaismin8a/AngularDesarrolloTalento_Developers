8653af61433a693402477aa992d54f9fecfc487e
Jazmin Ochoa Benitez <jazmin_2303@hotmail.com>
2023-09-06 11:58:28 -0700

    1ra Estructura del proyect

---
+++ src/app/components/regions/regions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regions',
+  templateUrl: './regions.component.html',
+  styleUrls: ['./regions.component.sass']
+})
+export class RegionsComponent {
+
+}
